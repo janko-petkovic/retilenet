f97a4a14de8c814325641b7dd20ab2bc5e0320f1
Janko Petkovic <j.petkovic91@gmail.com>
2021-09-20 15:16:53 +0200

    Add files via upload

---
+++ plot_accuracy_sweep.py
@@ -0,0 +1,129 @@
+import os, re
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#----------------------------------
+#              SETUP              |
+#---------------------------------- 
+
+
+# Parsing: dataset_name, retinic_kernel_size
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "dataset_name",
+    help="The name of the dataset on which the accuracy sweep has to be carried out",
+    type=str
+)
+
+parser.add_argument(
+    "retinic_kernel_size",
+    help="The retinic kernel size of the retinet to be used",
+    type=int
+)
+
+args = parser.parse_args()
+
+dataset_name = args.dataset_name
+kernel_size = args.retinic_kernel_size
+
+
+
+# IF YOU MOVE THE SCRIPT CHANGE ONLY THIS WITH NEW RELATIVE PATHS  <----------------------------------!!!
+LOAD_PATH = f"../results/accuracy_sweeps/{dataset_name}"
+SAVE_PATH = "../plots/accuracy_sweeps"
+
+
+
+
+# Import data: csv -> dataframe -> array
+mean_path = os.path.join(
+    LOAD_PATH, f"accuracy_sweep_DFC_RetiNet_{kernel_size}_vs_mean.csv")
+var_path = os.path.join(
+    LOAD_PATH, f"accuracy_sweep_DFC_RetiNet_{kernel_size}_vs_var.csv")
+
+results_mean = pd.read_csv(mean_path)
+results_var = pd.read_csv(var_path)
+
+printable_mean = np.array(results_mean)
+printable_var = np.array(results_var)
+
+
+
+
+#----------------------------------
+#             PLOTTING            |
+#---------------------------------- 
+
+
+# Chart config table
+
+ax_config = {
+    "data" : [
+        printable_mean,
+        printable_var
+    ],
+    "title" : [
+        f"{dataset_name} accuracy versus $\mu$ sweep",
+        f"{dataset_name} accuracy versus $\sigma$ sweep"
+    ],
+
+    "xlabel" : [
+        "$\mu$",
+        "$\sigma$"
+    ]
+}
+
+
+
+# Plotting loop
+
+fig, axs = plt.subplots(1,2)
+fig.set_size_inches(15,5)
+
+fig.tight_layout(
+    rect=[0.03, 0.1, 1, 0.9],
+    w_pad = 8,
+    h_pad = 5)
+
+
+for i, ax in enumerate(axs):
+    xyy = ax_config["data"][i]
+
+    # scatter the two charts
+    ax.scatter(xyy[:,0],xyy[:,1],
+        marker = 'o', facecolors="", edgecolors="black", label = "LeNet_5")
+
+    ax.scatter(xyy[:,0], xyy[:,2], 
+        marker = 'x', c="r", s=60, label = f"RetiNet_{kernel_size}")
+    
+    # y limits
+    ax.set_ylim([0,1])
+
+    # subplot title
+    ax.set_title(ax_config["title"][i], size = 18, pad = 20)
+
+    # tick dimension and padding
+    ax.tick_params(axis='x', labelsize=13, pad=10)
+    ax.tick_params(axis='y', labelsize=13, pad=10)
+
+    # axes labels
+    ax.set_xlabel(ax_config["xlabel"][i], fontsize=15, labelpad=15)
+    ax.set_ylabel("Accuracy", fontsize=15, labelpad=15)
+    
+    ax.legend(loc=8, fontsize=15)
+
+
+# Save the figure and show preview
+SAVE_PATH = os.path.join(
+    SAVE_PATH, 
+    f"{dataset_name}_{kernel_size}_accuracy_sweep.png"
+)
+
+plt.savefig(SAVE_PATH)
+
+plt.show()
---
+++ plot_retina.py
@@ -0,0 +1,73 @@
+import argparse
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from modules.models import DFC_RetiNet
+
+
+#----------------------------------
+#              SETUP              |
+#---------------------------------- 
+
+
+# Parsing: dataset_name, retinic_kernel_size
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "dataset_name",
+    help="The name of the dataset on which the accuracy sweep has to be carried out",
+    type=str
+)
+
+parser.add_argument(
+    "retinic_kernel_size",
+    help="The retinic kernel size of the retinet to be used",
+    type=int
+)
+
+args = parser.parse_args()
+
+dataset_name = args.dataset_name
+kernel_size = args.retinic_kernel_size
+
+
+# IF YOU MOVE THE SCRIPT CHANGE ONLY THIS WITH NEW RELATIVE PATHS  <----------------------------------!!!
+LOAD_PATH = f"../trained_models/{dataset_name}/trained_DFC_RetiNet_{kernel_size}_state_dict.pt"
+SAVE_PATH = f"../plots/accuracy_sweeps/retina/{dataset_name}_DFC_RetiNet_{kernel_size}_retina.png"
+
+# this line is pretty wrong
+in_channels = 3 if dataset_name=="SVHN" else 1
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = DFC_RetiNet(kernel_size, in_channels)
+model.load_state_dict(torch.load(LOAD_PATH, map_location=device))
+
+retina = model.retina.weight.detach().numpy()
+
+n = retina.shape[0]
+
+fig = plt.figure(figsize=(7,5))
+fig.set_dpi(157)
+
+fig.tight_layout(
+    rect=[0,0.05,0.95,0.95],
+    w_pad = 20,
+    h_pad = 0)
+
+
+plt.suptitle(f"{dataset_name} RetiNet {kernel_size}")
+
+for i, row in enumerate(retina):
+    for j, filter in enumerate(row):
+        fig.add_subplot(n,n,j+1+i*n)
+        plt.imshow(filter, cmap="gray")
+        
+        plt.xticks([])
+        plt.yticks([])
+
+        cbar = plt.colorbar(shrink=0.8)
+        cbar.ax.tick_params(labelsize=6) 
+
+plt.show()
